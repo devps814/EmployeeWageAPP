31fac326c7d39793ba2537b7fc771b12eec8257c
devps814 <dev.singh_cs22@gla.ac.in>
2026-07-10 15:30:23 +0530

    Applying use case 3 according to the task

--- employeeWage.js
+++ employeeWage.js
@@ -39,6 +39,20 @@ switch (empCheck) {
     console.log("Working Hours :", empHours);
     console.log("Daily Wage    : $" + dailyWage);
 
+    // UC3
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+
+        default:
+            return 0;
+    }
+}
+
 
 
 
