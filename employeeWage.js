a044b1a72e0601a8ed8cee253d7c7eb065fb2965
devps814 <dev.singh_cs22@gla.ac.in>
2026-07-10 15:07:39 +0530

    Applying UseCase1 for the give uce case

---
+++ employeeWage.js
@@ -0,0 +1,27 @@
+
+const IS_ABSENT = 0;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+
+const WAGE_PER_HOUR = 20;
+
+const MAX_WORKING_DAYS = 20;
+const MAX_WORKING_HOURS = 160;
+
+    let empCheck = Math.floor(Math.random() * 3);
+
+    let empHours = getWorkingHours(empCheck);
+
+    totalEmpHours += empHours;
+
+    let dailyWage = empHours * WAGE_PER_HOUR;
+
+    totalEmpWage += dailyWage;
+
+    console.log("Day :", totalWorkingDays);
+
+
+
